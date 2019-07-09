66f4b4d77d8e93fe7de7aec5a543bdb833c8a1b5
Nadezhda Zueva <zueva.nn@phystech.edu>
2019-07-09 15:19:07 +0300

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# export PATH=/usr/local/bin:$PATH\
+# brew install python\
+apt-get install python
+# export PATH=/usr/local/share/python:$PATH\
+# easy_install pip
+apt-get install python-pip
+pip install -r requirements.txt
+
